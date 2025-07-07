f3bd110066e5a0d81246c0ff6d4cb78a7387b0e4
Faizulafnaz <faizulafnaz22@gmail.com>
2025-07-07 15:26:12 +0530

    databse added

--- awaafi/settings.py
+++ awaafi/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,6 +151,12 @@ WSGI_APPLICATION = 'awaafi.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 
 # Password validation
