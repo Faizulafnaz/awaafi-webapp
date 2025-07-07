0600f971de5610d9236c53b1712192f3a80b4991
Faizulafnaz <faizulafnaz22@gmail.com>
2025-07-07 15:33:35 +0530

    build command updated

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate sites zero --noinput
-python manage.py migrate sites --fake --noinput
 python manage.py migrate sites --noinput
 python manage.py migrate --noinput
 python manage.py showmigrations
+python manage.py shell -c "from django.contrib.sites.models import Site; Site.objects.update_or_create(id=2, defaults={'domain': 'awaafi.onrender.com', 'name': 'Awaafi'})"
