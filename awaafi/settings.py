7b105f4055b6e77c81759bcb6f68025aeb0712bc
Faizulafnaz <faizulafnaz22@gmail.com>
2025-07-11 14:50:12 +0530

    changed to sqllite

--- awaafi/settings.py
+++ awaafi/settings.py
@@ -148,18 +148,18 @@ WSGI_APPLICATION = 'awaafi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     )
+# }
 
 
 # Password validation
