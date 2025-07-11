75be21153bf40ee51f82ef050874664e5e999e03
Faizulafnaz <faizulafnaz22@gmail.com>
2025-07-11 14:25:07 +0530

    command added to migrate the site app

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations
+python manage.py migrate sites
 python manage.py migrate
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@gamil.com').exists() or User.objects.create_superuser( 'admin@gmail.com', 'admin@123')"
 python manage.py showmigrations
