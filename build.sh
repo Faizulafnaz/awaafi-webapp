d2fb865ff9a9a7bfcfd3b723ad7b404728124e11
Faizulafnaz <faizulafnaz22@gmail.com>
2025-07-16 21:38:36 +0530

    build command updated

--- build.sh
+++ build.sh
@@ -2,11 +2,17 @@
 # exit on error
 set -o errexit
 
+set -o errexit
+
+pip install psycopg2-binary
+pip install dj-database-url
 pip install -r requirements.txt
+
 
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations
+python manage.py migrate sites zero --fake
 python manage.py migrate sites
 python manage.py migrate
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@gamil.com').exists() or User.objects.create_superuser( 'admin@gmail.com', 'admin@123')"
